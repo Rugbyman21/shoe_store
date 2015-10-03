0125a9126b88bc2b615fc485c826ec3859600e45
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-10-03 16:02:21 -0700

    prevent user from entering a blank name and capitalize first character in a word

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,11 @@
 class Brand < ActiveRecord::Base
-  has_and_belongs_to_many(:stores)
-  # validates(:name, {})
+  has_many(:stores)
+  validates(:name, {:presence => true, :length => { :minimum => 1 }})
+  before_save(:capitalize_brand)
+
+  private
+
+    define_method(:capitalize_brand) do
+      self.name=(name.capitalize)
+    end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe Brand do
-  describe '#stores' do
-    it {should have_and_belong_to_many(:stores)}
+  it("has many stores") do
+    test_brand = Brand.create({:name => "Nike"})
+    test_store1 = Store.create({:name => "Foot Locker", :brand_id => test_brand.id})
+    test_store2 = Store.create({:name => "Jonny Sole", :brand_id => test_brand.id})
+  end
+
+  it("prevents user from entering a blank name") do
+    brand = Brand.new({:name => ''})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it("ensures the length of name is at least 1 character long") do
+    brand = Brand.new({:name => ''.*(1)})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it("capitalizes the name") do
+    brand = Brand.create({:name => "nike"})
+    expect(brand.name()).to(eq("Nike"))
   end
 end
